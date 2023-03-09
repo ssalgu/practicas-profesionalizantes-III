1438a5f2ffb7d769b438cb71d781e50e9ee8a0d6
Sebastián Salgueiro <40528353+ssalgu@users.noreply.github.com>
2023-03-09 14:46:56 -0300

    Update calculate.php

--- single-page-application/calculadora_ver_05_final/backend/calculate.php
+++ single-page-application/calculadora_ver_05_final/backend/calculate.php
@@ -14,6 +14,7 @@ $input = json_decode( file_get_contents('php://input') );
 
 //------------ Model Functions ----------------
 
+// Función que ejecuta la sintaxis de la expresión en el display
 function calculate( $expression )
 {
 	$result = eval( 'return '.$expression.';' );
@@ -23,7 +24,10 @@ function calculate( $expression )
 
 //---------- Simulation of requested resource -------------
 
+// Invoca al método para ejecutar la sintaxis pasada por parámetro desde el model (como $input) 
 $response = calculate($input);
+// La respuesta ($response) es la promesa que reciben el llamado desdel el model (mediante .then),
+// con el resultado de la operación y que luego se asigna al display
 
 //-------------------- JSON Response ----------------------
 echo json_encode( 'Out of service' );
